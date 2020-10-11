f5e26e914f45088ced8c973b9a1e75b49443eaee
Michael-cuhk <michael.yunfei@gmail.com>
2020-10-11 18:28:19 +0800

    update java code for assignment 2

--- Assignment2/GeneralizedHarmonic.java
+++ Assignment2/GeneralizedHarmonic.java
@@ -1,1 +1,16 @@
 // Question 1 from CS with a purpose from Princeton
+
+public class GeneralizedHarmonic {
+    public static void main(String[] args){
+        int n = Integer.parseInt(args[0]);
+        int r = Integer.parseInt(args[1]);
+        double sum = 0.0;
+
+        for (int i = 1; i <= n; i++){
+            double tmp = 1/Math.pow(i, r);
+            sum = sum + tmp;
+
+        }
+        System.out.println(sum);
+    }
+}
--- Assignment2/RandomWalker.java
+++ Assignment2/RandomWalker.java
@@ -1,1 +1,33 @@
 // Question 3
+
+public class RandomWalker {
+    public static void main(String[] args){
+        int r = Integer.parseInt(args[0]);
+        int distance = 0;  // initialize the variable for measuring the distance
+        int left_x = 0;  // initialize the coordinate variable
+        int right_y = 0;
+        int step = 0;
+        while (distance < r){
+            if (Math.random() < 0.5){
+                if (Math.random() < 0.5) {
+                    right_y = right_y + 1;
+                }
+                else {
+                    left_x = left_x + 1;
+                }
+            }
+            else {
+                if (Math.random() < 0.5) {
+                    right_y = right_y - 1;
+                }
+                else {
+                    left_x = left_x - 1;
+                }
+            }
+            distance = Math.abs(left_x) + Math.abs(right_y);
+            System.out.println("(" + left_x + "," + " " + right_y + ")");
+            step++;
+        }
+        System.out.println("steps = " + step);
+    }
+}
